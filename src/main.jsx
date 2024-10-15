d981cfef9c700c903c3a9415cdfb3f8a61f71b7b
Ivan Lapickij <ivan.lapickij@gmail.com>
2024-10-15 23:36:18 +0100

    added Bootstrap
    added Bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AnimatedBackground } from 'animated-backgrounds';
+import Navbar from './components/Navbar';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AnimatedBackground animationName="starryNight" />
+  <Navbar/>
   </StrictMode>,
 )
