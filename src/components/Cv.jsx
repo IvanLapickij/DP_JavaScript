f566665391250af026a2a2abe736c9731b292674
Ivan Lapickij <ivan.lapickij@gmail.com>
2025-03-16 01:34:18 +0000

    added cv pdf

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -30,7 +30,7 @@ const Cv = () => {
       {/* Conditionally render the CV iframe for local PDF */}
       {showCv && (
         <iframe
-          src="src/components/Resume_Ivan_Lapickij.pdf"
+        src="/Resume_Ivan_Lapickij.pdf"
           width="100%"
           height="600px"
           title="Resume Ivan Lapickij"
